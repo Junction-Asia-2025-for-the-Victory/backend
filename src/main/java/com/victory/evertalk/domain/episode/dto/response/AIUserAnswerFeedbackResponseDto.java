47bb11a66e1bc9d224cd0c7eca88e6639b043c5d
WooYeonJoo <woojja07@naver.com>
2025-08-24 02:19:39 +0900

    hotfix: 역직렬화 에러 수정

--- src/main/java/com/victory/evertalk/domain/episode/dto/response/AIUserAnswerFeedbackResponseDto.java
+++ src/main/java/com/victory/evertalk/domain/episode/dto/response/AIUserAnswerFeedbackResponseDto.java
@@ -1,15 +1,21 @@
 package com.victory.evertalk.domain.episode.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Setter
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class AIUserAnswerFeedbackResponseDto {
 
     Integer affinity;
     String next_utterance;
     String emotion;
+    @JsonProperty("isGrammarError")
     boolean isGrammarError;
     String last_user_input;
     String last_user_correction_input;
